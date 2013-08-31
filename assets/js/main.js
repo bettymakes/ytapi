020e718568da4917e4d9374cadfdeafbf355a3fe
blibotics <blibotics@gmail.com>
2013-08-30 23:42:36 -0400

    Getting the data for each entry

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,17 @@ $(document).ready(function(){
   ytapi.videoList = $("#video-list");
 
 // Functions
-  ytapi.fetchApi = function(){
+  ytapi.fetchApi = function(type){
     var apiLink = "http://gdata.youtube.com/feeds/api/standardfeeds/most_popular?max-results=5&v=2&alt=json";
     $.getJSON(apiLink, function(data) {
       var feed = data.feed;
       var entries = feed.entry;
-      console.log(entries);
+
+      // for each entry, get its title, log it in console
+      $.each(entries, function(i, data){
+        console.log(data);     
+      
+      });
     });
   };
 
